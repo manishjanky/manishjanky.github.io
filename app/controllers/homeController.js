1c360414e22138c00fb94e7fd20dafb29c3470f3
manishjanky <manishjanky@gmail.com>
2016-12-26 14:29:46 +0530

    if service userProfile available
    if service userProfile available no need to fetch the profile.json again

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,13 +1,18 @@
 var homeController = manish.controller("homeController", function ($scope, $http, $location, $rootScope, profileService) {
     $scope.Username = 'manishjanky';
     $scope.userProfile = function () {
-        var promise = profileService.getUserProfile();
-        promise.then(function (response) {
-            $scope.user = response.data;
-            profileService.userProfile = response.data;
-        }, function (response) {
-            //error occured
-        })
+        if (profileService.userProfile != null) {
+            $scope.user = profileService.userProfile;
+        } else {
+            var promise = profileService.getUserProfile();
+            promise.then(function (response) {
+                $scope.user = response.data;
+                profileService.userProfile = response.data;
+            }, function (response) {
+                //error occured
+            })
+        }
+       
     }
     $scope.userProfile();
 });
