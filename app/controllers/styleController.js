222f65d52a48ec43efe41dfbae8bad869af1c561
Manish Kumar <manishjanky@gmail.com>
2016-12-27 14:53:06 +0530

    theme persistence even if page reloads

--- app/controllers/styleController.js
+++ app/controllers/styleController.js
@@ -1,7 +1,21 @@
 var styleController = manish.controller("styleController", function ($scope, $http, $location, $rootScope, $window) {
     $scope.styleSheet = "blue";
     $scope.updateStyle = function (style) {
+        if (angular.isUndefined(style)) {
+            //set and load the theme chosen by user using localstorage
+            var sty = $window.localStorage.getItem('manish_styleSheet');
+            if (angular.isUndefined(sty)) {
+                $scope.styleSheet = 'blue';
+            } else {
+                $scope.styleSheet = sty;
+            }
+
+        } else {
+            $scope.styleSheet = style;
+            $window.localStorage.setItem('manish_styleSheet', style);
+        }
         //update the style selected by the user
-        $scope.styleSheet = style;
+
     }
+    $scope.updateStyle();
 });
