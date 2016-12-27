6e85a83356b0e4c9cb6c8d70eb3c759a69f0d87c
Manish Kumar <manishjanky@gmail.com>
2016-12-27 16:39:26 +0530

    handle null value being returned form localstorage

--- app/controllers/styleController.js
+++ app/controllers/styleController.js
@@ -1,10 +1,10 @@
 var styleController = manish.controller("styleController", function ($scope, $http, $location, $rootScope, $window) {
     $scope.styleSheet = "blue";
     $scope.updateStyle = function (style) {
-        if (angular.isUndefined(style)) {
+        if (angular.isUndefined(style) || style==null) {
             //set and load the theme chosen by user using localstorage
             var sty = $window.localStorage.getItem('manish_styleSheet');
-            if (angular.isUndefined(sty)) {
+            if (angular.isUndefined(sty) || sty==null) {
                 $scope.styleSheet = 'blue';
             } else {
                 $scope.styleSheet = sty;
